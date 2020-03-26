0306c958a78e3a18bb3f2206a8bf6c048c17e28c
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-03-26 17:16:32 -0300

    Mobile da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -44,7 +44,7 @@ export default function Register() {
           <img src={logoImg} alt="Be The Hero" />
 
           <h1>Cadastro</h1>
-          <p>Faca seu cadastro, entre na plataforma e ajude pessoas a enconstrarem os casos da sua ONG.</p>
+          <p>Faca seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
